280d0fe04e85d59ae3043cfa5317c5de572efd12
Kajal Singh <136828939+Sssrii@users.noreply.github.com>
2025-06-20 11:39:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/FoodDel/', // 👈 Replace with your actual GitHub repo name
   plugins: [react()],
 })
